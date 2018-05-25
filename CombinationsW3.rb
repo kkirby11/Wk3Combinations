147e20a99394d2de08d683b7285a02c9cbc9f840
kristina kirby <kristinakirby@mac.com>
2018-05-25 10:07:32 -0600

    my clear message

---
+++ CombinationsW3.rb
@@ -0,0 +1,39 @@
+# Create a new ruby script
+# Add code to that ruby script to ask for user input
+# and output all possible combinations
+# Example string : 'dog'
+# Expected Output : d,do,dog,o,og,g
+# Have your script go in a loop or a recursive method call
+# so the user can keep inputting different words
+# Push to GitHub
+
+require 'pry'
+
+# def find_repeat
+def user_input
+#   @letters = Hash.new 0
+@letters = Hash.new ()
+#   puts "type something!"
+puts "type something and you will get something"
+#   input = gets.strip
+#   .split('').sort
+user_input = gets.split ('')
+#   input.each do |character| #defining the object
+user_input.each do |i| #go find this?
+#     # for each item
+    while user_input #each item
+#   @letters [character] += 1
+
+#   end
+    end
+#   puts @letters
+    puts 
+#   @letters.each do |key, value|
+#     puts "Nice! The letter #{key} is in your answer #{value} times!"
+#   end
+    end
+# end 
+end
+# find_repeat
+
+user_input
